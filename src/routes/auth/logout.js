752f6606b5fc8913b3ef647390a975afbefb90e2
Brian <bstanton773@gmail.com>
2022-09-08 15:29:49 -0500

    Small fix

--- src/routes/auth/logout.js
+++ src/routes/auth/logout.js
@@ -1,5 +1,5 @@
 module.exports = (req, res) => {
     // Clear the stored JWT token
     res.cookie('jwtToken', '', { maxAge: 900000, httpOnly: true })
-    res.redirect('auth/login')
+    res.redirect('/auth/login')
 }
